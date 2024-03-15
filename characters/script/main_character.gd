89a7668237a69ee53b7ba1c4776767060c0da376
Aluno EDU FOR 2 <alunofor2@edu.sesc-ce.com.br>
2024-03-15 11:36:41 -0300

    wip

--- characters/script/main_character.gd
+++ characters/script/main_character.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 class_name Character
 
 const _NORMAL_SPEED: float = 5.0
-const _SPRINT_SPEED: float = 9.0
+const _SPRINT_SPEED: float = 999.0
 
 var _current_speed: float
 
@@ -21,13 +21,13 @@ func _physics_process(_delta:float) -> void:
 func _move() -> void:
 	var _input_direction: Vector2 = Input.get_vector(
 		"move_left","move_right",
-		"move_forward","move_backward",	
+		"move_forward","move_backward",
 	)
 	
 	var _direction: Vector3 = transform.basis * Vector3(
 		_input_direction.x,
 		0,
-		_input_direction.y	
+		_input_direction.y
 	).normalized()
 	
 	is_running()
